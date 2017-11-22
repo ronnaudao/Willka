13f5e529e20d4db5753c75051f919ee487af506d
ronnaudao <32704673+ronnaudao@users.noreply.github.com>
2017-11-22 09:06:05 -0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <div class='indix' align="center">
 
 <?php
-  $login_cookie = $_COOKIE['login'];
+  @$login_cookie = $_COOKIE['login'];
     if(isset($login_cookie)){
       echo"Bem-Vindo, $login_cookie <br>";
       echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
